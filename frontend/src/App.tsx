08d7666a0f2e454c8f00fc4c512fe95e8eb0383c
iraja-baltezan <iraja.baltezan@gmail.com>
2022-09-12 02:19:41 -0300

    factor: add link to print and export

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import { DocContext } from './state/DocContextProvider';
+import { createStyles } from '@mantine/core';
 
 const appNavMenu = [
   { to: '/', title: 'Início' },
@@ -8,45 +9,57 @@ const appNavMenu = [
   { to: '/docs/new', title: 'Criar' },
 ];
 
+const useStyles = createStyles((theme) => ({
+  nav: {
+    display: 'flex',
+    gap: 8,
+  },
+  navLink: {
+    textDecoration: 'none',
+    padding: '0.5em',
+    borderBottom: 'none',
+    '&.-active': {
+      borderBottom: '1px solid #000',
+    }
+  }
+}))
+
 function App() {
   const { doc } = useContext(DocContext);
+  const { classes } = useStyles();
 
   return (
     <>
       <header className='-do-not-print'>
         Gerador de Formulários de Relatório de Inspeção
-        <nav
-          style={{
-            display: 'flex',
-            gap: 8,
-          }}
-        >
+        <nav className={classes.nav}>
           {appNavMenu
             .map((item, index) => (
               <NavLink
                 key={index}
                 to={item.to}
-                style={({ isActive }) => ({
-                  textDecoration: 'none',
-                  padding: '0.5em',
-                  borderBottom: isActive ? '1px solid #000' : 'none',
-                })}
+                className={state => classes.navLink + (state.isActive ? ' -active' : '')}
               >
                 {item.title}
               </NavLink>
             ))
           }
           {doc.id && (
-            <NavLink
-              to={`/docs/print/${doc.id}`}
-              style={({ isActive }) => ({
-                textDecoration: 'none',
-                padding: '0.5em',
-                borderBottom: isActive ? '1px solid #000' : 'none',
-              })}
-            >
-              Imprimir
-            </NavLink>
+            <>
+              <NavLink
+                to={`/docs/print/${doc.id}`}
+                className={state => classes.navLink + (state.isActive ? ' -active' : '')}
+              >
+                Imprimir
+              </NavLink>
+              <NavLink
+                to={`/docs/export/${doc.id}`}
+                className={state => classes.navLink + (state.isActive ? ' -active' : '')}
+              >
+                Exportar
+              </NavLink>
+
+            </>
           )}
         </nav>
       </header>
